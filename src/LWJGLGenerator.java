b66fca2c437c905e8438753d02ccd74f00e05685
Christian Stein <sormuras@gmail.com>
2022-06-27 19:52:56 +0200

    Group per module

--- src/LWJGLGenerator.java
+++ src/LWJGLGenerator.java
@@ -2,21 +2,29 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Map;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.function.UnaryOperator;
 
 public class LWJGLGenerator {
   public static void main(String... args) throws Exception {
+    var version = "3.3.1";
+    var generator =
+        new Generator(
+            version,
+            __ -> "https://repo.maven.apache.org/maven2",
+            __ -> "org.lwjgl",
+            module -> module.replace('.', '-'));
+
     var modules =
-        Set.of(
+        List.of(
             "lwjgl",
             "lwjgl.assimp",
             "lwjgl.bgfx",
-            "lwjgl.bom",
+            //  "lwjgl.bom",
             "lwjgl.cuda",
             "lwjgl.egl",
             "lwjgl.glfw",
@@ -56,39 +64,31 @@ public class LWJGLGenerator {
             "lwjgl.yoga",
             "lwjgl.zstd");
 
-    var generator =
-        new Generator(
-            "3.3.1",
-            modules,
-            __ -> "https://repo.maven.apache.org/maven2",
-            __ -> "org.lwjgl",
-            module -> module.replace('.', '-'));
-
     var classifiers =
-        Set.of(
-            // all
-            "",
+        List.of(
+            // all (platform-agnostic)
+            new Classifier("", ""),
             // linux
-            "natives-linux",
-            "natives-linux-arm32",
-            "natives-linux-arm64",
+            new Classifier("natives-linux", "linux"),
+            new Classifier("natives-linux-arm32", "linux-arm_32"),
+            new Classifier("natives-linux-arm64", "linux-arm_64"),
             // macos
-            "natives-macos",
-            "natives-macos-arm64",
+            new Classifier("natives-macos", "osx"),
+            new Classifier("natives-macos-arm64", "osx-arm_64"),
             // windows
-            "natives-windows",
-            "natives-windows-x86",
-            "natives-windows-arm64");
+            new Classifier("natives-windows", "windows-x86_32"),
+            new Classifier("natives-windows-x86", "windows-x86_64"),
+            new Classifier("natives-windows-arm64", "windows-arm_64")
+            //
+            );
 
     var http = HttpClient.newHttpClient();
-
-    for (var classifier : new TreeSet<>(classifiers)) {
-      System.out.println();
-      System.out.println('"' + classifier + '"');
-      var map = generator.generate(classifier);
-      for (var entry : map.entrySet()) {
-        var module = entry.getKey();
-        var uri = entry.getValue();
+    var lines = new ArrayList<String>();
+    for (var module : modules) {
+      System.out.println(module);
+      lines.add("# " + module);
+      for (var classifier : classifiers) {
+        var uri = generator.generateUri(module, classifier.library());
         var request = HttpRequest.newBuilder(URI.create(uri)).HEAD().build();
         var response = http.send(request, HttpResponse.BodyHandlers.discarding());
         if (response.statusCode() != 200) continue;
@@ -96,37 +96,30 @@ public class LWJGLGenerator {
         if (contentLength.isEmpty()) continue;
         var size = contentLength.getAsLong();
         if (size < 0) continue;
-        System.out.printf("%s -> %s#SIZE=%d%n", module, uri, size);
+        var empty = classifier.library().isEmpty();
+        var name = module + (empty ? "" : ".natives|" + classifier.normalized());
+        lines.add("%s=%s#SIZE=%d".formatted(name, uri, size));
       }
     }
+    var directory = Path.of("properties", "lwjgl");
+    Files.createDirectories(directory);
+    Files.write(directory.resolve("lwjgl@" + version + "-modules.properties"), lines);
   }
+
+  record Classifier(String library, String normalized) {}
 
   /** Generate (Java) Bill of Modules. */
   record Generator(
       String version,
-      Set<String> modules,
       UnaryOperator<String> repositoryGenerator,
       UnaryOperator<String> groupIdGenerator,
       UnaryOperator<String> artifactIdGenerator) {
 
-    Map<String, String> generate(String classifier) {
-      var map = new TreeMap<String, String>();
-      for (var module : modules) {
-        var repository = repositoryGenerator.apply(module);
-        var group = groupIdGenerator.apply(module).replace('.', '/');
-        var artifact = artifactIdGenerator.apply(module);
-        var uri = generateUri(repository, group, artifact, classifier);
-        map.put(module, uri + ".jar");
-      }
-      return map;
-    }
-
-    String generateFileName(String artifact, String classifier) {
-      var filename = new StringJoiner("-");
-      filename.add(artifact);
-      filename.add(version);
-      if (!classifier.isEmpty()) filename.add(classifier);
-      return filename.toString();
+    String generateUri(String module, String classifier) {
+      var repository = repositoryGenerator.apply(module);
+      var group = groupIdGenerator.apply(module).replace('.', '/');
+      var artifact = artifactIdGenerator.apply(module);
+      return generateUri(repository, group, artifact, classifier);
     }
 
     String generateUri(String repository, String group, String artifact, String classifier) {
@@ -136,7 +129,15 @@ public class LWJGLGenerator {
       uri.add(artifact);
       uri.add(version);
       uri.add(generateFileName(artifact, classifier));
-      return uri.toString();
+      return uri + ".jar";
+    }
+
+    String generateFileName(String artifact, String classifier) {
+      var filename = new StringJoiner("-");
+      filename.add(artifact);
+      filename.add(version);
+      if (!classifier.isEmpty()) filename.add(classifier);
+      return filename.toString();
     }
   }
 }
