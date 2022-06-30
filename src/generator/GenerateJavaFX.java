3b9a17b70a4285268094ab7c0dc01a99c04c9d5a
Christian Stein <sormuras@gmail.com>
2022-06-30 06:42:58 +0200

    Update JavaFX generator and BOMs

---
+++ src/generator/GenerateJavaFX.java
@@ -0,0 +1,57 @@
+package generator;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateJavaFX {
+
+  private static final String MAVEN_GROUP = "org.openjfx";
+
+  public static void main(String... args) throws Exception {
+    var version = args.length == 0 ? "18" : args[0];
+    var generator = LineGenerator.of("javafx@" + version);
+
+    var artifacts =
+        List.of(
+            new Artifact("javafx-base"),
+            new Artifact("javafx-controls"),
+            new Artifact("javafx-fxml"),
+            new Artifact("javafx-graphics"),
+            new Artifact("javafx-media"),
+            new Artifact("javafx-swing"),
+            new Artifact("javafx-web"));
+
+    var classifiers =
+        List.of(
+            // linux
+            new Classifier("linux-x86_64", "linux"),
+            new Classifier("linux-arm_64", "linux-aarch64"),
+            // macos
+            new Classifier("mac-x86_64", "mac"),
+            new Classifier("mac-arm_64", "mac-aarch64"),
+            // windows
+            new Classifier("windows-x86_32", "win-x86"),
+            new Classifier("windows-x86_64", "win"));
+
+    var lines = new ArrayList<String>();
+    lines.add("#");
+    lines.add("# JavaFX " + version);
+    lines.add("#");
+
+    for (var artifact : artifacts) {
+      lines.add("");
+      lines.add("# " + artifact.identifier());
+      for (var classifier : classifiers) {
+        var suffix = '|' + classifier.normalized();
+        var uri = Maven.central(MAVEN_GROUP, artifact.identifier(), version, classifier.identifier());
+        generator.generate(suffix, uri).ifPresent(lines::add);
+      }
+    }
+
+    var directory = Path.of("properties", "javafx");
+    Files.createDirectories(directory);
+    Files.write(directory.resolve("javafx@" + version + "-modules.properties"), lines);
+  }
+}
